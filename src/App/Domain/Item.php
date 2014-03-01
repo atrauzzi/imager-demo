6abc091774bb5e77ed1659cae5473283f5fdbd5f
Alexander Trauzzi <atrauzzi@gmail.com>
2014-03-01 07:39:51 -0600

    Using project to also demo out/test nested set stuff.

--- src/App/Domain/Item.php
+++ src/App/Domain/Item.php
@@ -1,14 +1,18 @@
 <?php namespace App\Domain {
 
-	use Illuminate\Database\Eloquent\Builder;
 	use Illuminate\Database\Eloquent\Model;
+	use Illuminate\Database\Eloquent\Builder;
 	use TippingCanoe\Imager\Model\Imageable;
 	use TippingCanoe\Imager\Model\ImageableImpl;
+	use Atrauzzi\LaravelNestedSet\NestedSet;
+	use Atrauzzi\LaravelNestedSet\NestedSetImpl;
 
 
-	class Item extends Model implements Imageable {
+	class Item extends Model implements Imageable, NestedSet {
 
 		use ImageableImpl;
+		use NestedSetImpl;
+
 
 		protected $table = "item";
 
